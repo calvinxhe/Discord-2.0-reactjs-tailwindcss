d27b0e2ed3c083fef5d0185770dbd3640ca6d763
calvinxhe <herb.builder@gmail.com>
2023-10-09 12:37:15 -0400

    firebase doesn't work on deployment but does on em ulator

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { Provider } from "react-redux";
 import { store } from "./app/store";
 
-ReactDOM.render(
+const root = createRoot(document.getElementById("root"));
+root.render(
   <React.StrictMode>
     <Provider store={store}>
       <App />
     </Provider>
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
